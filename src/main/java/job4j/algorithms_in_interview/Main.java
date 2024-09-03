08967d2b5d4846f76f9e3835f2ed97ae6a2c5879
IvanPavlovets <ivan150287@gmail.com>
2024-09-03 12:16:53 +0700

    Aлгоритм двух указателей [#505153]

---
+++ src/main/java/job4j/algorithms_in_interview/Main.java
@@ -0,0 +1,47 @@
+package job4j.algorithms_in_interview;
+
+import java.util.Arrays;
+
+/**
+ * Алгоритм двух укказателей:
+ * Дан отсортированный массив от большего к меньшему.
+ * Нужно возвести каждый элемент в квадрат, чтобы результирующий массив был тоже отсортирован.
+ * Двигаемсяся по массиву с двух сторон: с начала и с конца,
+ * возводя элементы в квадрат и помещая их в результирующий массив.
+ * Шаги алгоритма:
+ * 1. Создайте результирующий массив той же длины, что и исходный массив.
+ * 2. Установите два указателя: один указывает на начало исходного массива, а другой - на его конец.
+ * 3. Начните двигаться по массиву с обоих концов, возводя элементы в квадрат и помещая их
+ * в результирующий массив.
+ * 4. Следите за тем, чтобы значения в результирующем массиве шли от большего к меньшему.
+ * Это решение будет иметь временную сложность O(n), где n - количество элементов в исходном массиве,
+ * так как мы пройдемся только один раз по каждому элементу массива.
+ */
+public class Main {
+    public static int[] squareSortedArray(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+        int left = 0, right = n - 1;
+        int resultIndex = n - 1;
+
+        while (left <= right) {
+            if (Math.abs(arr[left]) > Math.abs(arr[right])) {
+                result[resultIndex] = arr[left] * arr[left];
+                left++;
+            } else {
+                result[resultIndex] = arr[right] * arr[right];
+                right--;
+            }
+            resultIndex--;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArray = {-4, -2, 0, 2, 3, 5};
+        int[] resultArray = squareSortedArray(sortedArray);
+        System.out.println(Arrays.toString(sortedArray));
+    }
+
+}
---
+++ src/main/java/job4j/algorithms_in_interview/SmallestRangeFinder.java
@@ -0,0 +1,67 @@
+package job4j.algorithms_in_interview;
+
+
+import java.util.Arrays;
+
+/**
+ * Алгоритм нахождение наименьшего диапазона с данным количеством различных элементов.
+ * Есть отсортированный массив целых чисел и число k.
+ */
+public class SmallestRangeFinder {
+
+    /**
+     * Добавьте реализацию метода здесь
+     * Метод должен принимать массив nums и целочисленное значение k,
+     * и возвращать массив из двух целых чисел, представляющих наименьший диапазон
+     * с k различными элементами в массиве nums
+     * Если такой диапазон найти невозможно, вернуть null
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int[] findSmallestRange(int[] nums, int k) {
+        int[] result = new int[k];
+        int left = 0, right = 1;
+        int resultIndex = 0;
+        int[] range = new int[2];
+        int[] main = nums;
+        int offset = 0;
+
+        while (resultIndex <= k) {
+            if (right > nums.length - 1) {
+                return null;
+            }
+            if (nums[left] < nums[right]) {
+                result[resultIndex] = nums[left];
+                resultIndex++;
+                result[resultIndex] = nums[right];
+                if (resultIndex == result.length - 1) {
+                    break;
+                }
+                left++;
+                right++;
+            } else {
+                nums = Arrays.copyOfRange(nums, right, nums.length);
+                offset = 1;
+                left = 0;
+                right = 1;
+                resultIndex = 0;
+            }
+        }
+        range[0] = Arrays.binarySearch(main, result[offset]) - offset;
+        range[1] = Arrays.binarySearch(main, result[result.length - 1]);
+        return range;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 7, 9};
+        int k = 3;
+        int[] result = findSmallestRange(nums, k);
+        if (result != null) {
+            System.out.println("Наименьший диапазон с " + k + " различными элементами: " + Arrays.toString(result));
+        } else {
+            System.out.println("Такой диапазон не существует.");
+        }
+    }
+
+}
